4c616caddc75c4f1a072ff087f3a264641633270
pc-de-lisa <lisa.aubry@insa-cvl.fr>
2014-11-26 20:43:29 +0100

    msg

--- main.h
+++ main.h
@@ -12,6 +12,31 @@
 #include "grille.h"
 #include "libgraphique.h"
 
+
+	/********************
+	**Identifiants......  Survie........Taille...Taille_du_bide.....Saut_max..metabolisme...gestation........frequen_reproduction
+	**espece vide.......0  10 000	   -1        0                	0         0		0	 	 10 000
+	**plancton .........1  10 000       1        0 			0	  0		1	 	 4 	
+	**corail............2  2            1        1			1    	  1	  	1	 	 4	
+	**bar...............3  3 	    2        2			2	  1		1        	 4
+	**thon..............4  5            2        1       		3         2		1        	 4
+	**pollution.........5  10 000       0        1			1         0		0	 	 10 000
+	**pyranha...........6  3            2        3			3	  2		2 	 	 4
+	**requin............7  4            4        3			2         2		2 	 	 4
+	**baleine...........9  3            5        2			2   	  2		2 	 	 4
+	**pecheur...........10 10 000       3        0			1         0		0	 	 10 000
+	**pont..............11 10 000       2        0			0         0		0	 	 10 000
+	*********************/
+
+static const int duree_survie[]={10000, 10000, 2, 3, 5, 10000, 3, 4, 4, 3, 10000, 10000};
+static const int taille[]={-1, 1, 1, 2, 2, 0, 2, 4, 4, 5, 3, 2};
+static const int taille_du_bide[]={0, 0, 1, 2, 1, 1, 3, 3, 4, 2, 0, 0};
+static const int saut_max[]={ 0, 0, 1, 2, 3, 0, 3, 2, 2, 2, 1, 0};
+static const int metabolisme[]={0, 0, 1, 1, 2, 0, 2, 2, 2, 0, 0};
+static const int gestation[]={0, 1, 1, 1, 1, 0, 2, 2, 2, 0, 0};
+static const int frequence_reproduction[]={10000, 4, 4, 4, 4, 10000, 4, 4, 4, 10000, 10000};
+
+
 int rand_a_b(int a, int b);
 
 #endif
